c60d3700c179b58c84f696d886bed1bdf8fdd3c8
KercyDing <dkx215417@gmail.com>
2026-05-29 21:02:48 +0800

    Update .config/nvim/lua/config/autocmds.lua

--- private_dot_config/nvim/lua/config/autocmds.lua
+++ private_dot_config/nvim/lua/config/autocmds.lua
@@ -17,3 +17,13 @@ vim.api.nvim_create_autocmd("FileType", {
   end,
 })
 
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "qf", "help" },
+  callback = function(event)
+    vim.keymap.set("n", "q", "<cmd>close<CR>", {
+      buffer = event.buf,
+      desc = "Close window",
+    })
+  end,
+})
+
